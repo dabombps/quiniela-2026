1dadb96f40d4fb980c2b256470fe4f10b2ce37d6
dabombps <70433899+dabombps@users.noreply.github.com>
2026-05-23 22:48:49 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import App from './App2022';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<React.StrictMode><App /></React.StrictMode>);
