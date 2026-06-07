b500594bd4fa1bfa1b8fc4e8863846a0c89efc8c
dabombps <70433899+dabombps@users.noreply.github.com>
2026-06-07 23:12:09 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,28 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App2022 from './App2022';
 import App2026 from './App2026';
-import App2026Test from './App2026Test';
 
+// Por defecto carga Familia, hash cambia entre quinielas
 function getApp() {
   const hash = window.location.hash.replace('#', '');
-  if (hash.startsWith('/test'))    return { Component: App2026Test, props: {} };
-  if (hash.startsWith('/familia')) return { Component: App2026, props: { quinielaId: 'familia' } };
-  if (hash.startsWith('/amigos'))  return { Component: App2026, props: { quinielaId: 'amigos' } };
-  return { Component: App2022, props: {} };
+  if (hash.startsWith('/amigos')) return { quinielaId: 'amigos' };
+  return { quinielaId: 'familia' }; // default
 }
 
 function Router() {
-  const [current, setCurrent] = React.useState(getApp);
-
+  const [props, setProps] = React.useState(getApp);
   React.useEffect(() => {
-    const onHash = () => setCurrent(getApp());
+    const onHash = () => setProps(getApp());
     window.addEventListener('hashchange', onHash);
     return () => window.removeEventListener('hashchange', onHash);
   }, []);
-
-  const { Component, props } = current;
-  return <Component {...props} />;
+  return <App2026 {...props} />;
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
